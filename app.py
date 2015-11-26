c781280f57177cef8ae916c87a47bc0ad2bb76bc
Tim Yousaf <tyousaf@palantir.com>
2015-11-26 15:21:14 -0500

    Basic filter for Uber & Seamless transactions.

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 @app.route("/api/transactions")
 def transactions():
 	transactions = mint.get_transactions()
-	return transactions.to_json()
+	uber_and_seamless_transactions = transactions[ transactions.description.str.contains("Uber|Seamless") ]
+	return uber_and_seamless_transactions.to_json()
 
 if __name__ == "__main__":
     creds_file =  open("/Users/tyousaf/mint.txt")
