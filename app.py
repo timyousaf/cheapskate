0da5c0f0d21890726d3eadb9c62889fa0fed70b0
Tim Yousaf <timyousaf@gmail.com>
2015-11-27 20:34:29 -0500

    Backend returns ultra-hacky histogram of expenses : month

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import json
 import mintapi
+import datetime
 
 app = Flask(__name__)
 
@@ -14,8 +15,30 @@ def transactions():
 	uber_and_seamless_transactions = transactions[ transactions.description.str.contains("Uber|Seamless") ]
 	return uber_and_seamless_transactions.to_json(orient='records')
 
+@app.route("/api/transactions/histogram")
+def histogram():
+	# ULTRA HACK, blargh
+	# TODO: figure out pandas histogram
+	transactions = mint.get_transactions()
+	uber_and_seamless_transactions = transactions[ transactions.description.str.contains("Uber|Seamless") ]
+	actual = uber_and_seamless_transactions.to_json(orient='records')
+	actual = json.loads(actual)
+	histogram = {}
+	key = 'date'
+	for trans in actual:
+		trans[key] = datetime.datetime.fromtimestamp(trans[key] / 1000 ).strftime('%Y-%m')
+		if trans[key] in histogram:
+			histogram[trans[key]] += int(trans['amount'])
+		else:
+			histogram[trans[key]] = int(trans['amount'])
+	array = []
+	for key in histogram:
+		array.append( { 'date' : key, 'value' : histogram[key] } )
+	return json.dumps(array)
+
 if __name__ == "__main__":
     creds_file =  open("/Users/tyousaf/mint.txt")
     creds =  json.loads( creds_file.read() )
     mint = mintapi.Mint( creds['email'], creds['password'] )
     app.run(debug=True)
+    
